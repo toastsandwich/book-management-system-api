fe90ab6fed00f44aefd1d5c6b317c655137f569e
toastsandwich <shreyasxcore2003@gmail.com>
2024-02-19 01:00:09 +0530

    added logic

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -6,7 +6,7 @@ import "github.com/jinzhu/gorm"
 var db *gorm.DB
 
 func Connect() {
-	d, err := gorm.Open("mysql", "gomon:pass/clients?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "gomon:pass/books?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
